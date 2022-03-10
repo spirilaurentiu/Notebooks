9fddfd4591d2f70f747ce22d31dcd6667fc0c6ea
Laurentiu Spiridon <spiridon.laurentiu@gmail.com>
2022-03-10 11:47:21 +0200

    Added simulate.py for ala10.

--- simulate.py
+++ simulate.py
@@ -1,15 +1,19 @@
+# Imports
+import os
 from robosample import *
 
 # Load Amber files
-prmtop = AmberPrmtopFile("mproA/ligand.prmtop")
-inpcrd = AmberInpcrdFile("mproA/ligand.rst7")
+prmtop = AmberPrmtopFile("ala10/ligand.prmtop")
+inpcrd = AmberInpcrdFile("ala10/ligand.rst7")
 
 # Hardware platform
 platform = Platform.getPlatformByName('GPU')
-
 properties={'nofThreads': 0}
 
 # Create a Robosample system by calling createSystem on prmtop
+if os.path.exists("robots"):
+  shutil.rmtree("robots")
+
 system = prmtop.createSystem(createDirs = True,
 	nonbondedMethod = "CutoffPeriodic",
  	nonbondedCutoff = 1.44*nanometer,
@@ -24,20 +28,22 @@ system = prmtop.createSystem(createDirs = True,
 integrator = HMCIntegrator(300*kelvin,   # Temperature of head bath
                            0.001*picoseconds) # Time step
 
-
+# Create Simulation object
 simulation = Simulation(prmtop.topology, system, integrator, platform, properties)
 simulation.reporters.append(PDBReporter('temp', 1))
 simulation.context.setPositions(inpcrd.positions)
 
 ## Generate NMA-Scaled Flex Files
-sAll = [[1, 306]]
-#simulation.addWorld(regionType='roll', region=s1_5,  rootMobility='Free', timestep=0.003, mdsteps=10, argJointType='Pin', subsets=['rama'], samples=1)
-#simulation.addWorld(regionType='stretch', region=nter,  rootMobility='Free', timestep=0.004, mdsteps=40, argJointType='Pin', subsets=['rama', 'side'], samples=6)
-simulation.addWorld(regionType='stretch', region=sAll,  rootMobility='Weld', timestep=0.004, mdsteps=5, argJointType='Pin', subsets=['rama','side'], samples=10, contacts=False, contactCutoff=2)
-#simulation.addWorld(regionType='coils', region=s1_278,  rootMobility='Free', timestep=0.002, mdsteps=5, argJointType='Pin', subsets=['all'], samples=9, contacts=True, contactCutoff=2)
-#simulation.addWorld(regionType='loops', rootMobility='Free', timestep=0.004, mdsteps=30, argJointType='Pin', subsets=['rama'], samples=1)
-#simulation.addWorld(regionType='accesible', rootMobility='Weld', timestep=0.004, mdsteps=30, argJointType='Pin', subsets=['side'], samples=1)
+sAll = [[1, 10]]
+
+simulation.addWorld(regionType='stretch',
+                    region=sAll,
+                    rootMobility='Weld',
+                    timestep=0.004,
+                    mdsteps=5,
+                    argJointType='Pin',
+                    subsets=['rama','side'],
+                    samples=1)
 
 # run simulation
 simulation.step(5)
-
